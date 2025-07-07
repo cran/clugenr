a1789c6bc2082b38c19eade635c9bc965fbfea0d
Nuno Fachada <faken@fakenmc.com>
2025-07-07 19:00:06 +0000

    version 1.0.4

--- inst/doc/dev.R
+++ inst/doc/dev.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -11,7 +11,7 @@ knitr::opts_chunk$set(
 RNGversion("3.6.0")
 set.seed(12345)
 
-## ---- out.width = "100%", dpi = 200-------------------------------------------
+## ----out.width = "100%", dpi = 200--------------------------------------------
 library(clugenr)
 x <- clugen(2, 5, 800, c(-1, 1), 0.6, c(4, 6), 5, 0.4, 0.5)
 plot(x$points, col = x$clusters, xlab = "x", ylab = "y", asp = 1)
--- tests/testthat/test_lintr.R
+++ tests/testthat/test_lintr.R
@@ -1,8 +1,8 @@
-# Copyright (c) 2020-2023 Nuno Fachada
+# Copyright (c) 2020-2025 Nuno Fachada
 # Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
 test_that("Package linting", {
-  # Don't do linting on CRAN, coverage reporting or testing on CI + Not Windows
+  # Don't do linting on CRAN or coverage reporting
   skip_if(is_test_mode("cran"))
   skip_on_cran()
   skip_on_covr()
@@ -15,9 +15,6 @@ test_that("Package linting", {
   lintr::expect_lint_free(
     path = pkgpath,
     linters = lintr::linters_with_defaults(
-      # The clugen() function has a large cyclomatic complexity
-      # so let's increase this a bit
-      cyclocomp_linter = lintr::cyclocomp_linter(complexity_limit = 60),
       # The official line length for this package is 80, but lets
       # give it some margin; also, Windows complains about the 80
       # limit due to not recognizing some UTF-8 characters.
